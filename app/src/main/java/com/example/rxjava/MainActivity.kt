41ab391ae088b873e962d4f0e73c2a9387f4411e
Diego Tobar <dyegote26@gmail.com>
2022-04-28 17:05:38 -0500

    inicial 2

--- app/src/main/java/com/example/rxjava/MainActivity.kt
+++ app/src/main/java/com/example/rxjava/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.rxjava
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    private fun simpleOperation(){
+
+        val observable: Observable<String> = Observable.create<String>{
+            Log.d(TAG, "Observable - Thread: ${Thread.currentThread().name}")
+            it.onNext("Hola")
+            it.onNext("Recodigo")
+            it.onNext("Fin")
+            it.onComplete()
+        }
+
+    }
+
+    val observer = object: Observer<String>{
+        override fun onSubscribe(d: Disposable) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onNext(t: String) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onError(e: Throwable) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onComplete() {
+            TODO("Not yet implemented")
+        }
+
+
+    }
+
+    companion object{
+        private const val TAG = "MainActivity"
     }
 }
